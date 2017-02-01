9356406a3326cbdb9e0c60b1d17cb2709ec5632f
Viktor Khokhryakov <viktor.khokhryakov@gmail.com>
2017-02-01 14:41:16 +0400

    created base class for actions

---
+++ src/BaseLongPollAction.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace izumi\longpoll;
+
+use Yii;
+use yii\base\Action;
+use yii\base\InvalidConfigException;
+use yii\web\Response;
+
+/**
+ * Base class for long poll actions.
+ * Please extend this class for creating complex actions.
+ * @author Viktor Khokhryakov <viktor.khokhryakov@gmail.com>
+ */
+class BaseLongPollAction extends Action
+{
+    /**
+     * @var array
+     */
+    public $events;
+    /**
+     * @var callable
+     */
+    public $callback;
+    /**
+     * @var string
+     */
+    public $serverClass = 'izumi\longpoll\Server';
+
+    /**
+     * @return Response
+     * @throws InvalidConfigException
+     */
+    protected function runInternal()
+    {
+        if (!is_callable($this->callback)) {
+            throw new InvalidConfigException('"' . get_class($this) . '::callback" should be a valid callback.');
+        }
+        /** @var Server $server */
+        $server = Yii::createObject([
+            'class' => $this->serverClass,
+            'events' => $this->events,
+        ]);
+        $server->run();
+        if ($server->getTriggeredEvents()) {
+            call_user_func($this->callback, $server);
+        }
+
+        return $server->getResponse();
+    }
+}
--- src/LongPollAction.php
+++ src/LongPollAction.php
@@ -2,9 +2,6 @@
 
 namespace izumi\longpoll;
 
-use Yii;
-use yii\base\Action;
-use yii\base\InvalidConfigException;
 use yii\web\Response;
 
 /**
@@ -33,40 +30,14 @@ use yii\web\Response;
  *
  * @author Viktor Khokhryakov <viktor.khokhryakov@gmail.com>
  */
-class LongPollAction extends Action
+class LongPollAction extends BaseLongPollAction
 {
-    /**
-     * @var array
-     */
-    public $events;
-    /**
-     * @var callable
-     */
-    public $callback;
-    /**
-     * @var string
-     */
-    public $serverClass = 'izumi\longpoll\Server';
 
     /**
      * @return Response
-     * @throws InvalidConfigException
      */
     public function run()
     {
-        if (!is_callable($this->callback)) {
-            throw new InvalidConfigException('"' . get_class($this) . '::callback" should be a valid callback.');
-        }
-        /** @var Server $server */
-        $server = Yii::createObject([
-            'class' => $this->serverClass,
-            'events' => $this->events,
-        ]);
-        $server->run();
-        if ($server->getTriggeredEvents()) {
-            call_user_func($this->callback, $server);
-        }
-
-        return $server->getResponse();
+        return $this->runInternal();
     }
 }
