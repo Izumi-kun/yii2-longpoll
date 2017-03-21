405f005a96e060ff8ced5c57f61db77d6025ca9a
Viktor Khokhryakov <viktor.khokhryakov@gmail.com>
2017-03-21 19:47:48 +0400

    Shutdown when the client is gone

--- src/Server.php
+++ src/Server.php
@@ -94,6 +94,9 @@ class Server extends Object
         $endTime = time() + $this->timeout;
         do {
             $triggered = [];
+            if (connection_aborted()) {
+                break;
+            }
             foreach ($events as $eventKey => $event) {
                 $event->updateState();
                 if ($event->getState() !== $lastStates[$eventKey]) {
