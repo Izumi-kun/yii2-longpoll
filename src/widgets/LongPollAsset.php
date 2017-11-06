835edf72099e6342e25cdce1be34ee36abd75e8c
Viktor Khokhryakov <viktor.khokhryakov@gmail.com>
2017-11-06 13:40:55 +0400

    added copyright

--- src/widgets/LongPollAsset.php
+++ src/widgets/LongPollAsset.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * @link https://github.com/Izumi-kun/yii2-longpoll
+ * @copyright Copyright (c) 2017 Viktor Khokhryakov
+ * @license http://opensource.org/licenses/BSD-3-Clause
+ */
 
 namespace izumi\longpoll\widgets;
 
